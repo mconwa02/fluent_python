aa9ba64f2a062148f06083fcd46a0735456530d2
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2022-06-25 19:35:17 +0100

    adding suits and spade list/fucntion

--- data_collections/special_methods.py
+++ data_collections/special_methods.py
@@ -24,13 +24,16 @@ print(my_collection_dict.__getitem__("Y"))
 # Example 1-1 __getitem__ and __len__ methods
 Card = namedtuple("Card", ["rank", "suit"])
 # namedtuple builds classes of objects are just bundles of attributes with no custom methods.
+# >>> beer_card = Card('7', 'diamonds')
+# >>> beer_card
+# Card(rank='7', suit='diamonds')
 
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11) + list("JQKA")]
-    suits = "spades diamonds clubs hearts".split()
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
 
-    def __int__(self):
+    def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
@@ -40,4 +43,12 @@ class FrenchDeck:
         return self._cards[position]
 
 
-print(Card)
+deck = FrenchDeck()
+print(len(deck))
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
